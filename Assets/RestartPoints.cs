8190e25fefde95e73e1f35a79c614c8f6c075d02
LandingHeart <land.liu0123@gmail.com>
2020-09-18 19:51:01 -0400

    restart point

---
+++ Assets/RestartPoints.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartPoints : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    [SerializeField]
+    private Transform cachePlayerPostion;
+
+    void Start()
+    {
+      
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.position = cachePlayerPostion.position;
+        }
+    }
+}
