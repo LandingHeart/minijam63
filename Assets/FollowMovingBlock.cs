2b6e2cf2ccbf83e525c80d1cc2817805c88c2f1d
wwkb123 <wwkb1233@gmail.com>
2020-09-20 17:34:23 -0400

    adjust level6

---
+++ Assets/FollowMovingBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMovingBlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void checkIsOnMovingBlock(){
+        RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, layermask);
+        Color rayColor;
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.red;
+        }
+        else
+        {
+            rayColor = Color.green;
+        }
+        Debug.DrawRay(transform.position, Vector2.down * 1.1f, rayColor);
+        // Debug.Log(raycastHit.collider.name);
+        return raycastHit.collider != null;
+    }
+}
