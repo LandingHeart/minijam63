a47a79d0d0920f36437ec4e4619041ce20f19e1d
wwkb123 <wwkb1233@gmail.com>
2020-09-20 15:51:29 -0400

    add scene manager

---
+++ Assets/SceneManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManagerScript : MonoBehaviour
+{   
+    Scene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = gameObject.scene;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)){  // reload scene
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
+    
+}
