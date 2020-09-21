de5dd477206ee53149e059bb4a0195567ace89a0
wwkb123 <wwkb1233@gmail.com>
2020-09-20 23:20:38 -0400

    make cloud static

---
+++ Assets/CloudScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+    private static CloudScript instance;
+    void Awake(){
+        if(instance != null){
+            Destroy(gameObject);
+        }else{
+            instance = this;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        
+    }
+}
